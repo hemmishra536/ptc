43f6f590fa9e1bb5b22a2ffc53360afe5462c493
hemmishra536 <105469847+hemmishra536@users.noreply.github.com>
2023-10-28 18:17:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,195 @@
+// Dynamic Quiz App with randomly chosen multiple choice questions
+// Design and development by Micke Berg
+
+const main = document.querySelector('main');
+const theQuestion = document.querySelector('.the-question');
+const optionsFieldContainer = document.querySelector('.options-field');
+const optionField = document.querySelector('.options-field').children;
+const answerTrackerContainer = document.querySelector('.answers-tracker');
+const questionsOfTotal = document.querySelector('.question-of-total');
+const totalQuestionSpan = document.querySelector('.total-question');
+const totalQuestionSpan2 = document.querySelector('.total-question2');
+const makeChoice = document.querySelector('.make-choice');
+const correctAnswerSpan = document.querySelector('.correct-answers');
+const percentage = document.querySelector('.percentage');
+const gameOver = document.querySelector('.game-over');
+const option = document.querySelector('.option');
+const allQuestionsArr = [
+  {
+    q: '123',
+    options:['A','B','C', 'D'],
+    answer: 0
+  },
+  
+];
+
+let questionIndex = 0;
+let index = 0;
+let myArray = [];
+let myArr = [];
+let score = 0;
+
+let allQuestions = [...allQuestionsArr];
+let theRandomQuestions = [];
+
+function selectQuestions() {
+  if (allQuestions.length < 5) {
+    allQuestions = [...allQuestionsArr];
+  };
+  
+  theRandomQuestions = [];
+
+  for (let i = 0; i < 5; i++) {
+    let randomQuestion = Math.floor(Math.random() * allQuestions.length);
+
+    theRandomQuestions.push(allQuestions[randomQuestion]);
+    allQuestions.splice(randomQuestion, 1);
+  };
+};
+
+function load() {
+  questionsOfTotal.innerHTML = index+1;
+  optionsFieldContainer.innerHTML = '';
+  theQuestion.innerHTML = theRandomQuestions[questionIndex].q;
+
+  for(let i = 0; i < theRandomQuestions[questionIndex].options.length; i++) {
+    optionsFieldContainer.innerHTML += `<div class="option">${theRandomQuestions[questionIndex].options[i]}</div>`;
+  };
+
+  const parent = optionsFieldContainer;
+  const children = Array.from(parent.children);
+
+  children.map((option, index) => {
+    option.addEventListener('click', () => {
+      checkIfRightAnswer(index);
+    });
+  })
+};
+
+function next() {
+  validate();
+};
+
+function checkIfRightAnswer(index) {
+  if(index == theRandomQuestions[questionIndex].answer) {
+    optionField[index].classList.add('correct');
+    updateAnswerTracker('correct');
+    score++;
+  } else {
+    optionField[index].classList.add('wrong');
+    optionField[theRandomQuestions[questionIndex].answer].classList.add('correct');
+    updateAnswerTracker('wrong');
+  }
+  disableOptions();
+  
+  makeChoice.innerHTML = '';
+  makeChoice.style.backgroundColor = '';
+};
+
+function disableOptions() {
+  for(let i = 0; i < optionField.length; i++) {
+    optionField[i].classList.add('disabled');
+  }
+};
+
+function enableOptions() {
+  for(let i = 0; i < optionsFieldContainer.children.length; i++) {
+    optionField[i].classList.remove('disabled', 'correct', 'wrong');
+  }
+};
+
+function validate() {
+  if(!optionField[0].classList.contains('disabled')) {
+    makeChoice.innerHTML = 'Välj ett alternativ innan du kan gå vidare.';
+    makeChoice.style.backgroundColor = 'rgba(255, 255, 255, 0.415)';
+  }
+  else {
+    if (questionIndex === 4) {
+      quizOver();
+      questionIndex = 0;
+    } else {
+      enableOptions();
+      questionIndex++;
+      load();
+    }
+  }
+};
+
+function answerTracker() {
+    for(let i = 0; i < theRandomQuestions.length; i++){ 
+      const div = document.createElement('div');
+        answerTrackerContainer.appendChild(div);
+  }
+}
+
+function updateAnswerTracker(classNam) {
+  let boxes = Array.from(answerTrackerContainer.children);
+  boxes[index].classList.add(classNam);
+  index++;
+};
+
+function tryAgain() {
+  index = 0;
+  answerTrackerContainer.innerHTML = '';
+  document.querySelector('.quiz-over').classList.remove('show');
+  score = 0;
+  loadRound();
+};
+
+window.onload = function () {
+  loadRound();
+};
+
+function loadRound() {
+  selectQuestions();
+  load();
+  
+  totalQuestionSpan.innerHTML = theRandomQuestions.length;
+
+  answerTracker();
+  index = 0;
+};
+
+const gameOverStatements = [
+  { option: 'Öhh... Hallå! Inte sova på lektionerna!!' },
+  { option: 'Det går inget vidare för dig va?!! Dags att göra läxorna!!'},
+  { option: 'Hmm... det går väl så där. Kanske plugga lite mer!!'},
+  { option: 'Helt ok, men bättre kan du nog!' },
+  { option: 'Du verkar ju ha ganska bra koll på det här med demokrati!!' },
+  { option: 'Ooooh, grym du är. Du är ju en demokratiexpert!!' },
+  { option: 'Testa dina kunskaper igen!!' }
+];
+
+function quizOver() {
+  document.querySelector('.quiz-over').classList.add('show');
+
+  let finishScore = (score/theRandomQuestions.length)*100;
+  
+  correctAnswerSpan.innerHTML = score;
+  totalQuestionSpan2.innerHTML = theRandomQuestions.length;
+  percentage.innerHTML = (score/theRandomQuestions.length)*100 + '%';
+
+  switch (finishScore) {
+    case 0:
+      gameOver.innerHTML = gameOverStatements[0].option;
+      break;
+    case 20:
+      gameOver.innerHTML = gameOverStatements[1].option;
+      break;
+    case 40:
+      gameOver.innerHTML = gameOverStatements[2].option;
+      break;
+    case 60:
+      gameOver.innerHTML = gameOverStatements[3].option;
+      break;
+    case 80:
+      gameOver.innerHTML = gameOverStatements[4].option;
+      break;
+    case 100:
+      gameOver.innerHTML = gameOverStatements[5].option;
+      break;
+    default:
+      gameOver.innerHTML = gameOverStatements[6].option;
+  }
+};
+
